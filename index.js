918bdc081a64b35e4ebe910c35eac5fd574e8b90
Mike Seese <seesemichaelj@gmail.com>
2021-03-30 03:21:49 -0700

    feat: support namespaces other than Filecoin (#10)

--- index.js
+++ index.js
@@ -15,9 +15,20 @@ class LotusRPC {
         } else if (prop === 'then') {
           return undefined
         } else {
-          const method = prop.charAt(0).toUpperCase() + prop.slice(1)
+          let method = prop.charAt(0).toUpperCase() + prop.slice(1)
           const schemaMethod = schema.methods[method]
           if (schemaMethod) {
+            if (schemaMethod.namespace && method.startsWith(schemaMethod.namespace)) {
+              // Namespaces other than "Filecoin" are permitted such that name of the
+              // `schemaMethod` provided in the schema is `NamespaceMethod` for the respective
+              // `Namespace.Method` method. Further, the `schemaMethod` needs to provide
+              // the `namespace` string set to `Namespace`. This allows calling
+              // provider.namespaceMethod() or provider.NamespaceMethod().
+              method = `${schemaMethod.namespace}.${method.slice(schemaMethod.namespace.length)}`;
+            } else {
+              method = `Filecoin.${method}`;
+            }
+
             if (schemaMethod.subscription) {
               return this.callSchemaMethodSub.bind(this, method, schemaMethod)
             } else {
@@ -35,7 +46,7 @@ class LotusRPC {
   async callSchemaMethod (method, schemaMethod, ...args) {
     await this.provider.connect()
     const request = {
-      method: `Filecoin.${method}`
+      method
     }
     request.params = args
     return this.provider.send(request, schemaMethod)
@@ -44,7 +55,7 @@ class LotusRPC {
   callSchemaMethodSub (method, schemaMethod, ...args) {
     // await this.provider.connect()
     const request = {
-      method: `Filecoin.${method}`
+      method
     }
     const cb = args[0]
     request.params = args.slice(1)
